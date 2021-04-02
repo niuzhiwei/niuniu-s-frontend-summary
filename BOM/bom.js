3d764416a030df99b626e75c524c0f75b8e8d894
niuzhiwei <xujin02@qq.com>
2021-04-02 18:40:09 +0800

    BOM

---
+++ BOM/bom.js
@@ -0,0 +1,19 @@
+//navigator
+const ua = navigator.userAgent
+const isChrome = ua.indexOf('Chorme')
+console.log(isChrome)
+
+//screen
+console.log(screen.width)
+console.log(screen.height)
+
+//location
+console.log(location.href)
+console.log(location.protocol) //http https
+console.log(location.pathname) // learn/199
+console.log(location.search)
+console.log(location.hash)
+
+//history
+history.back()
+history.forward()
